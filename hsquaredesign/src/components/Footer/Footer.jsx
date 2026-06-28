40e4307c17e71cdb8a1413bb2b02342ad7f2fa3f
surendartk <surendar.tkb@gmail.com>
2026-06-28 15:03:49 +0530

    check

--- hsquaredesign/src/components/Footer/Footer.jsx
+++ hsquaredesign/src/components/Footer/Footer.jsx
@@ -90,7 +90,7 @@ function Footer() {
         </div>
 
         <div className="footer-bottom">
-          <p>© 2026 H SQUARE DESIGN. All Rights Reserved.</p>
+          <p>© 2026 HSQUAREDESIGN. All Rights Reserved.</p>
         </div>
       </div>
     </footer>
